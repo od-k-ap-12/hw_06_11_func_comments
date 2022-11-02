d8f58f1d0319f3c39b0d2eb013309cb6102481de
od-k-ap-12 <112928816+od-k-ap-12@users.noreply.github.com>
2022-11-02 02:50:05 +0200

    Update WindowsProject1.cpp

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -7,16 +7,17 @@
 #define MAX_LOADSTRING 100
 
 // Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HINSTANCE hInst;                                // current instance //Текущее состояние
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text //Текст заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name //Название класса главного окна
 
 // Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ATOM                MyRegisterClass(HINSTANCE hInstance); //Регистрация класса окна
+BOOL                InitInstance(HINSTANCE, int); //Инициализация окна
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM); //Обработка сообщений окна
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM); //Окно About
 
+//Точка входа в программу
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -33,6 +34,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
+    //Инициализация приложения. Если не сработало, возвращает false.
     if (!InitInstance (hInstance, nCmdShow))
     {
         return FALSE;
@@ -43,6 +45,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MSG msg;
 
     // Main message loop:
+  //Главный цикл сообщений
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -62,6 +65,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 //  PURPOSE: Registers the window class.
 //
+//Регистрация класса окна
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -93,13 +97,15 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
+//Инициализация окна
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
-
+  //Создание окна
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
+  
+//Возвращает false, если не вышло создать окно
    if (!hWnd)
    {
       return FALSE;
@@ -115,12 +121,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
 //  PURPOSE: Processes messages for the main window.
-//
+// Обработка сообщений главного окна
 //  WM_COMMAND  - process the application menu
 //  WM_PAINT    - Paint the main window
 //  WM_DESTROY  - post a quit message and return
 //
 //
+//Обработка сообщений главного окна
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -129,12 +136,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             int wmId = LOWORD(wParam);
             // Parse the menu selections:
+          //Проверяет, какое именно событие произошло
             switch (wmId)
             {
             case IDM_ABOUT:
+                //Вызов диалогового окна при нажатии на About
                 DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
             case IDM_EXIT:
+                //Закрытие окна при нажатии на Exit
                 DestroyWindow(hWnd);
                 break;
             default:
@@ -142,6 +152,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+        //Отрисовка окна при перемещении
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -151,6 +162,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_DESTROY:
+        //Возвращаемое значение при закрытии окна
         PostQuitMessage(0);
         break;
     default:
@@ -159,16 +171,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+//Обработка сообщений окна About
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
+        //Инициализация диалогового окна
     case WM_INITDIALOG:
         return (INT_PTR)TRUE;
 
     case WM_COMMAND:
+        //Закрывает диалоговое окно при нажатии на ок или отмену
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
             EndDialog(hDlg, LOWORD(wParam));
